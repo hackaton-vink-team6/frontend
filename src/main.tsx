5a37f791d49fcd807bacea3280c06d5489e82581
gineff <canone@inbox.ru>
2024-03-29 17:13:36 +0300

    add alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/index.scss'
+//import './styles/index.scss'
 import { AppContextProvider } from './hooks/useAppContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
