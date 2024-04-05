26b4d8b8ca1bba2f44e6f2587cae3d0f8556567e
Evelina Kim <like17seventeen@gmail.com>
2024-04-05 13:12:27 +0300

    feat: взять компоненты typography, button из ui, поправить modal

--- src/components/FeedbackForm/feedback-form.tsx
+++ src/components/FeedbackForm/feedback-form.tsx
@@ -1,4 +1,9 @@
-import { useState, useId, FormEvent } from 'react';
+import React, { useState } from 'react';
+import { useId } from 'react';
+import { FormEvent } from 'react';
+import { Button } from '@/components/UI/Button';
+import { Typography } from '@/components/UI/Typography';
+
 import feedbackStyles from './feedback-form.module.scss';
 
 interface IFeedbackFormProps {
@@ -16,35 +21,39 @@ export const FeedbackForm: React.FC<IFeedbackFormProps> = ({
     e.preventDefault();
   };
   return (
-    <form className={feedbackStyles.form} onSubmit={onSubmit}>
-      <label className={feedbackStyles.text} htmlFor={textAreaId}>
-        Опишите, что вам не понравилось:
-      </label>
-      <textarea
-        id={textAreaId}
-        name="opinion"
-        cols={30}
-        rows={5}
-        className={feedbackStyles.textarea}
-        value={opinion}
-        onChange={(e) => setOpinion(e.target.value)}
-      ></textarea>
-      <div className={feedbackStyles.buttons}>
-        <button
-          onClick={handleCancelClick}
-          type="button"
-          className={feedbackStyles.button}
-        >
-          Отменить
-        </button>
-        <button
-          disabled={!opinion}
-          type="submit"
-          className={feedbackStyles.button}
-        >
-          Отправить
-        </button>
-      </div>
-    </form>
+    <>
+      <Typography htmlTag="h1" size="s" weight="bold">
+        Почему ответ не помог?
+      </Typography>
+      <hr className={feedbackStyles.hr} />
+      <form className={feedbackStyles.form} onSubmit={onSubmit}>
+        <label className={feedbackStyles.text} htmlFor={textAreaId}>
+          Опишите, что вам не понравилось:
+        </label>
+        <textarea
+          id={textAreaId}
+          name="opinion"
+          cols={30}
+          rows={5}
+          className={feedbackStyles.textarea}
+          value={opinion}
+          onChange={(e) => setOpinion(e.target.value)}
+        />
+        <div className={feedbackStyles.buttons}>
+          <Button
+            text="Отменить"
+            view="secondary"
+            className={feedbackStyles.button}
+            onClick={handleCancelClick}
+          />
+          <Button
+            text="Отправить"
+            view="secondary"
+            className={feedbackStyles.button}
+            disabled={!opinion}
+          />
+        </div>
+      </form>
+    </>
   );
 };
--- src/components/UI/Modal/modal.tsx
+++ src/components/UI/Modal/modal.tsx
@@ -1,17 +1,14 @@
 import { ReactNode } from 'react';
 import { Icon } from '@components/UI/Icon';
-
 import modalStyles from './modal.module.scss';
 
 interface IModalProps {
   handleClosingModal: () => void;
   children: ReactNode;
-  title: string;
 }
 
 export const Modal: React.FC<IModalProps> = ({
   handleClosingModal,
-  title,
   children,
 }): JSX.Element => {
   return (
@@ -23,8 +20,6 @@ export const Modal: React.FC<IModalProps> = ({
         >
           <Icon type="close" />
         </button>
-        <h1 className={modalStyles.title}>{title}</h1>
-        <hr className={modalStyles.hr} />
         {children}
       </div>
     </div>
